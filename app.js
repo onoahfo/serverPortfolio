81f93389a4fee4f7f3c40e3f06c916fab3642f26
Noah <onoahfo@gmail.com>
2019-06-26 06:35:33 -0400

    added basic server frame

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+//middleware here
+app.set("view engine", "ejs");
+
+app.get('/home', function (req, res) {
+    res.render('home');
+});
+ 
+app.listen(3000, function(err){
+    if(err){
+        console.log(err)
+    }
+    console.log('Server is live on port 3000')
+});
